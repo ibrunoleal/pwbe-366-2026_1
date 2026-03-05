3c8a01b4bc69e5e062756d3527851c5a5215239e
ibrunoleal <ibrunoleal@gmail.com>
2026-03-05 15:17:18 -0300

    modified:   README.md 	modified:   main.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pwbe-366-2026_1
 Projeto com exemplos basicos para a disciplina PWBE da turma 366 de 2026-1.
+
+Parte 2: criação de rotas.
--- main.py
+++ main.py
@@ -4,13 +4,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Funcao de tratamento da requisicao. Pode pegar e tratar parametros e 
+# Funcao de tratamento da requisicao. Pode pegar e tratar parametros e
 # deve obrigatoriamente retornar uma resposta (response).
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
+def hello_aluno():
+    return "Ola aluno!"
+
+
+def hello_professor():
+    return "Ola professor!"
+
+
 # Regra de roteamento.
 app.add_url_rule(
     rule="/hello", endpoint="hello", view_func=hello_world, methods=["GET"]
+)
+app.add_url_rule(
+    rule="/professor/hello",
+    endpoint="hello_professor",
+    view_func=hello_professor,
+    methods=["GET"],
+)
+app.add_url_rule(
+    rule="/aluno/hello",
+    endpoint="hello_aluno",
+    view_func=hello_aluno,
+    methods=["GET"],
 )
